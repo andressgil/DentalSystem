dbefb9d76f2c1777e073fda87bec6ecf91c5f06d
dolly lasso <catherinelasso@yahoo.com>
2022-12-07 22:23:57 -0500

    Se realizaron cambios y elimino User solo quedo patient

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express"
+import mongoose from "mongoose";
 import patientRouter from "./routes/patientRouter.js";
-import userRouter from "./routes/userRouter.js";
+
 
 const app = express();
 
@@ -10,6 +11,14 @@ app.listen(port, () => {
     console.log("El Servidor se esta ejecutando correctamente. ");
 })
 
+mongoose.connect("mongodb+srv://Syden:Syden@clustersyden.zoku9s3.mongodb.net/?retryWrites=true&w=majority", (err)=>{
+    if(err){
+        console.log(err);
+    }else{
+        console.log("La base de datos se encuentra conectada. ");
+    }
+    
+})
+
 app.use(express.json())
-app.use("/user", userRouter)
 app.use("/patient", patientRouter)
--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,1 +1,70 @@
 import patientModel from "../models/patientModel.js"
+
+//POST
+export async function createPatient(req, res){
+
+    const paciente = req.body.paciente
+
+    let documento
+
+    try {
+        documento = await patientModel.create(paciente)
+    } catch (error) {
+        res.status(400).json(error.message)
+        return
+    }
+
+    res.status(201).json(paciente)
+}
+
+// //READ
+// export async function readPatient(req, res) {
+//     //implementacion aqui  
+//     const id = req.body.id
+
+//     let documento
+
+//     try {
+//         documento = await patientModel.findOne({"_id": id})
+//     } catch (error) {
+//         res.status(400).json(error.message)
+//         return
+//     }
+
+//     res.status(200).json(documento)
+// }
+
+// //UPDATE
+// export async function updatePatient(req, res) {
+  
+//     const cedula = req.params.cedula
+//     const updates = req.body.updates
+
+//     let documento
+
+//     try {
+//         documento = await patientModel.updateOne({"_id":id}, updates)//dentro de (la primer peticion es q busq ese id, con el segudo es q haga el cambio)
+//     } catch (error) {
+//         res.status(400).json(error.message)
+//         return
+//     }
+   
+//     res.status(200).json(documento)
+// }
+
+// //DELETE
+// export async function deletePatient(req, res) {
+//     //implementacion aqui 
+//     const id = req.body.id
+  
+//     let documento = null
+
+//     try {
+//         documento = await patientModel.deleteOne({"_id": id})
+//     } catch (error) {
+//         res.status(400).json(error.message)
+//         return
+//     }
+   
+//     res.status(200).json(documento)
+// }
