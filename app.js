814ac5342e7d7fcc87fa3c32e29fc942d42a6570
andressgil <yheison.gil.mt@usa.edu.co>
2022-12-09 23:37:43 -0500

    Login

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express"
 import mongoose from "mongoose";
 import patientRouter from "./routes/patientRouter.js";
 import quotesRouter from "./routes/quotesRouter.js";
+import authenticationRouter from "./routes/authenticationRouter.js";
 
 
 const app = express();
@@ -11,6 +12,8 @@ const port = process.env.PORT || 8080
 app.listen(port, () => {
     console.log("El Servidor se esta ejecutando correctamente. ");
 })
+
+
 
 mongoose.connect("mongodb+srv://SydenApp:SydenApp@clustersydenapp.heb9m49.mongodb.net/syden-app?retryWrites=true&w=majority", (err)=>{
     if(err){
@@ -24,3 +27,4 @@ mongoose.connect("mongodb+srv://SydenApp:SydenApp@clustersydenapp.heb9m49.mongod
 app.use(express.json())
 app.use("/patient", patientRouter)
 app.use("/quotes", quotesRouter)
+app.use("/login", authenticationRouter)
