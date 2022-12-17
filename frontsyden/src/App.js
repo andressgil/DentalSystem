209fece0852eed2c1b61f54df9fb805ae2786ef2
francisco <Fvergaraperdomo89@correo.unicordoba.edu.co>
2022-12-16 22:13:11 -0500

    se creo carpeta de back y front separados

---
+++ frontsyden/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./assets/global.css"
+import Login from './components/Login';
+
+
+export default function App() {
+    return (<Login></Login>)
+}
--- models/authenticationModel.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const authenticationModel = mongoose.Schema({
-    "user":{ type: String, unique: true, required: true},
-    "password":{ type: String, unique: true, required: true}
- })
-
-export default mongoose.model("credentials", authenticationModel)
