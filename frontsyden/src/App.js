ea312b56e2965b4a6037eb5f2e5d3e8692d19b7f
Jhonny Sarmiento <jhonny.sarmiento@o365.unab.edu.co>
2022-12-17 15:59:39 -0500

    actualizacion

--- frontsyden/src/App.js
+++ frontsyden/src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import "./assets/global.css"
-import Login from './components/Login';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Formulario } from "./components/Formulario";
+import "./assets/global.css";
+import Login from "./components/Login";
 
-
-export default function App() {
-    return (<Login></Login>)
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Login />} />
+        <Route path="/formulario" exact element={<Formulario />} />
+      </Routes>
+    </Router>
+  );
 }
+export default App;
