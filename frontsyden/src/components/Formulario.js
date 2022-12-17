9d28e97f53f9adab8ee622faeb54aa3c44714d6a
Jhonny Sarmiento <jhonny.sarmiento@o365.unab.edu.co>
2022-12-17 16:03:33 -0500

    primera actualizacion del formulario de inscripcion pacientes

---
+++ frontsyden/src/components/Formulario.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Formulario = () => {
+  return (
+    <main>
+      <h1>Formulario de Inscripcion</h1>
+      <Link to={"/"}> Regresar</Link>
+    </main>
+  );
+};
+export { Formulario };
